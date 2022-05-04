8f016dfd2583b817208943922202fc84bf1ea6a0
hsoffos <huntersoffos@gmail.com>
2022-05-04 18:46:48 -0400

    recreating Add Two Numbers

---
+++ Add Two Numbers.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    #Initialize LinkedList
+    def __init__(self):
+        self.head = None
+
+# Input: l1 = [2,4,3], l2 = [5,6,4]
