ced4b69395abe9083c45590fa723026eedd9a9e2
hsoffos <huntersoffos@gmail.com>
2022-05-04 19:50:47 -0400

    Completed: Solved Add Two Numbers

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -5,6 +5,12 @@ class ListNode(object):
         self.next = next
 
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+
 # Input: l1 = [2,4,3], l2 = [5,6,4]
 if __name__ == '__main__':
     # empty list plus nodes
@@ -21,11 +27,34 @@ if __name__ == '__main__':
     l2.next = l2second
     l2second.next = l2third
 
-    while l1 or l2:
-        val1 = l1.val
-        val2 = l2.val
-        print(val1, val2)
-        l1 = l1.next if l1 else None
-        l2 = l2.next if l2 else None
 
+# You are given two non-empty linked lists representing two non-negative integers.
+# The digits are stored in reverse order, and each of their nodes contains a single digit.
+# Add the two numbers and return the sum as a linked list.
+#
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
+# l1 = [2, 4, 3] & l2 = [5, 6, 4]
+class Solution(object):
+    def addTwoNumbers(self, ll1, ll2):
+
+        carry = 0
+        out = ListNode(0)
+        last = out
+
+        while ll1 or ll2 or carry:
+            val1 = ll1.val if ll1 else 0
+            val2 = ll2.val if ll2 else 0
+            carry, r = divmod(val1+val2+carry, 10)
+            #print(divmod(val1+val2+carry,10))
+
+            last.next = ListNode(r)
+            last = last.next
+
+            ll1 = ll1.next if ll1 else None
+            ll2 = ll2.next if ll2 else None
+            print(last.val)
+        return last.next
+
+solve = Solution()
+finalsolve = solve.addTwoNumbers(l1, l2)
